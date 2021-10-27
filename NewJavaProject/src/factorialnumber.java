8e6f7f205967edbacd7ebb8c5d6c9f55bc7845d7
Administrator <Administrator@Admin-PC>
2021-10-27 16:20:31 +0530

    final commit

---
+++ NewJavaProject/src/factorialnumber.java
@@ -0,0 +1,18 @@
+
+public class factorialnumber {
+
+	public static void main(String[] args) 
+	{
+		int a=7;  
+		int fact=1;
+		for (int i=1;i<a;i++) {
+			fact=fact*i;	
+		}
+		int b=5;
+		int Fact=1;
+		for(int i=b;i>0;i--) {
+			Fact=Fact*i;
+		}
+   System.out.println("Factorial of a is " +fact);
+	}
+}
